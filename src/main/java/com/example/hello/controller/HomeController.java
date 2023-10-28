a295eb50a41f187e7f0c8dccbe43a06e67ee5b67
Rexcy-Roy <rexcyroy135@gmail.com>
2023-10-28 12:11:59 +0530

    second changes

---
+++ src/main/java/com/example/hello/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.hello.controller;
+
+import com.example.hello.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+    private final GreetingService greetingService;
+    @Autowired
+    public HomeController(GreetingService greetingService){
+      this.greetingService=greetingService;
+    }
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World";
+    }
+
+}
---
+++ src/main/java/com/example/hello/service/GreetingService.java
@@ -0,0 +1,7 @@
+package com.example.hello.service;
+
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
